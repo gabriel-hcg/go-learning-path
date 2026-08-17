b3cf34c05f6c3caf1dec2fd945a1134f0145f5ab
Gabriel <100551978+gabriel-hcg@users.noreply.github.com>
2026-08-17 09:13:15 -0300

    Add fundamentals_05.go for custom type example
    Implement a new Go program demonstrating custom type usage.

---
+++ fundamentals/src/fundamentals_05.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type newjeans int
+
+var x newjeans
+var y int
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Printf("%v\n", x)
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T/n", y)
+}
