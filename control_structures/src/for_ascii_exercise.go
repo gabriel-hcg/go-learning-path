6a37487805725466e635f48228c4d38e4d9776b5
Gabriel <100551978+gabriel-hcg@users.noreply.github.com>
2026-08-19 02:09:25 -0300

    Comment out ASCII exercise code in for_ascii_exercise.go
    Commented out the exercise code demonstrating ASCII conversion.

--- control_structures/src/for_ascii_exercise.go
+++ control_structures/src/for_ascii_exercise.go
@@ -8,3 +8,17 @@ func main() {
 		fmt.Println()
 	}
 }
+
+/* 
+Exercise showing just the numerical code and converting to string to show the character
+package main
+
+import "fmt"
+
+func main() {
+	for x := 33; x <= 122; x++ {
+		fmt.Printf("%d - %v", x, string(x))
+		fmt.Println()
+	}
+}
+*/
