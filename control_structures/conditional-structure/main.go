faf446f5083625723f3e39825573823fca27fa73
Gabriel <100551978+gabriel-hcg@users.noreply.github.com>
2026-08-25 23:46:27 -0300

    Add example of conditional structure in Go
    This code demonstrates a simple conditional structure in Go that checks a boolean variable and prints a message if true.

---
+++ control_structures/conditional-structure/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	segasaturn := true
+	if segasaturn {
+		fmt.Println("セガサターン城！")
+	}
+}
+
+/* The exercise above exemplifies how a simple conditional 
+structure in Go works. This exercise checks if the variable `segasaturn` 
+is true and if it is, it prints the message "セガサターン城！(Play Sega Saturn)" to the console. */
